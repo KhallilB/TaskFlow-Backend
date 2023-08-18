126e4b90bfa414d67721039cb9bfb884c8878652
KhallilB <khallilbailey@gmail.com>
2023-08-18 14:53:25 -0700

    move mocked data

--- src/controllers/projects/projects.test.ts
+++ src/controllers/projects/projects.test.ts
@@ -3,18 +3,7 @@ import mongoose from "mongoose";
 import app from "../../app";
 import Project from "../../models/Project/Project";
 
-const userData = {
-  username: "testuser",
-  firstName: "John",
-  lastName: "Doe",
-  email: "test@example.com",
-  password: "Testpassword2@",
-};
-
-const projectData = {
-  name: "Test Project",
-  description: "Test Project Description",
-};
+import { mockUserData, mockProjectData } from "../../test/mock";
 
 describe("Project Functional Tests", () => {
   let token: string;
@@ -24,7 +13,7 @@ describe("Project Functional Tests", () => {
 
     const response = await request(app)
       .post("/api/v1/auth/register")
-      .send(userData);
+      .send(mockUserData);
 
     token = response.body.token;
   });
@@ -38,13 +27,13 @@ describe("Project Functional Tests", () => {
     const response = await request(app)
       .post("/api/v1/projects/create")
       .set("Authorization", `Bearer ${token}`)
-      .send(projectData);
+      .send(mockProjectData);
 
     expect(response.status).toBe(201);
     expect(response.body.success).toBe(true);
     expect(response.body.data).toBeDefined();
-    expect(response.body.data.name).toBe(projectData.name);
-    expect(response.body.data.description).toBe(projectData.description);
+    expect(response.body.data.name).toBe(mockProjectData.name);
+    expect(response.body.data.description).toBe(mockProjectData.description);
   });
 
   it("should throw error on project creation", async () => {
@@ -61,7 +50,7 @@ describe("Project Functional Tests", () => {
   });
 
   afterAll(async () => {
-    await Project.deleteOne({ name: projectData.name });
+    await Project.deleteOne({ name: mockProjectData.name });
     await mongoose.connection.close();
   })
 });
---
+++ src/test/mock.ts
@@ -0,0 +1,12 @@
+export const mockUserData = {
+  username: "testuser",
+  firstName: "John",
+  lastName: "Doe",
+  email: "test@example.com",
+  password: "Testpassword2@",
+};
+
+export const mockProjectData = {
+  name: "Test Project",
+  description: "Test Project Description",
+};
