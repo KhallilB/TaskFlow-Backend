725dec49f2e83afb1e5803e19f5304196c7e3453
KhallilB <khallilbailey@gmail.com>
2023-08-19 01:10:28 -0700

    get specific project

--- src/controllers/projects/projects.ts
+++ src/controllers/projects/projects.ts
@@ -2,42 +2,59 @@ import { Request, Response, NextFunction } from "express";
 import Project from "../../models/Project/Project";
 
 export const createProject = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const { name, description } = req.body;
-        const createdBy = (<any>req).user?._id;
+  try {
+    const { name, description } = req.body;
+    const createdBy = (<any>req).user?._id;
 
-        const project = await Project.create({
-            name,
-            description,
-            createdBy,
-        });
+    const project = await Project.create({
+      name,
+      description,
+      createdBy,
+    });
 
-        return res.status(201).json({
-            success: true,
-            data: project,
-        });
-    } catch (error: any) {
-        next(error);
-    }
+    return res.status(201).json({
+      success: true,
+      data: project,
+    });
+  } catch (error: any) {
+    next(error);
+  }
 };
 
 export const getProjects = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const projects = await Project.find({ createdBy: (<any>req).user?._id });
+  try {
+    const projects = await Project.find({ createdBy: (<any>req).user?._id });
 
-        return res.status(200).json({
-            success: true,
-            data: projects,
-        });
-    } catch (error: any) {
-        next(error);
-    }
-}
+    return res.status(200).json({
+      success: true,
+      data: projects,
+    });
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const getProject = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const project = await Project.findById(req.params.id);
+
+    return res.status(200).json({
+      success: true,
+      data: project,
+    });
+  } catch (error: any) {
+    next(error);
+  }
+};
--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import isUserAuthorized from "../middleware/isUserAuthorized/isUserAuthorized";
-import { createProject, getProjects } from "../controllers/projects/projects";
+import { createProject, getProjects, getProject } from "../controllers/projects/projects";
 
 const router = Router();
 
@@ -9,5 +9,6 @@ router.use("/projects", router);
 // Private Routes
 router.post("/create", isUserAuthorized, createProject);
 router.get("/", isUserAuthorized, getProjects);
+router.get("/:id", isUserAuthorized, getProject);
 
 export default router;
