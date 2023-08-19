29dbbcce129cb193ddef50af945e19280a8dcf9a
KhallilB <khallilbailey@gmail.com>
2023-08-19 09:38:33 -0700

    router project user assignment

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -7,10 +7,14 @@ const router = Router();
 router.use("/projects", router);
 
 // Private Routes
+// Project CRUD
 router.post("/create", isUserAuthorized, createProject);
 router.get("/", isUserAuthorized, getProjects);
 router.get("/:id", isUserAuthorized, getProject);
 router.put("/:id", isUserAuthorized, updateProject);
 router.delete("/:id", isUserAuthorized, deleteProject);
+
+// Project Assignment
+router.post("/:projectId/assign/user/:userId", isUserAuthorized, createProject);
 
 export default router;
